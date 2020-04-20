5ff0cb15849057358ce2c663e6054198be315a74
Rimantas <belauzas@yahoo.com>
2020-04-20 12:26:30 +0300

    Work gallery #21

---
+++ js/data/works-data.js
@@ -0,0 +1,49 @@
+const data = [
+    {
+        photo: 'serlokas.jpg',
+        name: 'Serlokas',
+        categories: ['html', 'css', 'basic']
+    },
+    {
+        photo: 'saskes.jpg',
+        name: 'Saskes',
+        categories: ['HTML', 'css', 'basic']
+    },
+    {
+        photo: 'mergaite.jpg',
+        name: 'Mergaite',
+        categories: ['html', 'css', 'Basic']
+    },
+    {
+        photo: 'signup.jpg',
+        name: 'Sign up',
+        categories: ['html', 'css', 'responsive', 'basic']
+    },
+    {
+        photo: 'comingsoon.jpg',
+        name: 'Coming soon',
+        categories: ['html', 'css', 'responsive', 'medium']
+    },
+    {
+        photo: 'chessboard.jpg',
+        name: 'Chessboard',
+        categories: ['html', 'css', 'js', 'medium']
+    },
+    {
+        photo: 'facebook.jpg',
+        name: 'Facebook',
+        categories: ['html', 'css', 'js', 'medium']
+    },
+    {
+        photo: 'minesweeper.jpg',
+        name: 'Minesweeper',
+        categories: ['html', 'css', 'js', 'advanced']
+    },
+    {
+        photo: 'portfolio.jpg',
+        name: 'Portfolio',
+        categories: ['html', 'css', 'js', 'responsive', 'advanced']
+    }
+];
+
+export default data;
